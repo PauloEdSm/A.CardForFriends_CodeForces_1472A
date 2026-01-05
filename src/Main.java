9e0f5251c06e486f787a8c2b84b410245041a917
paulodev <pauloeduardotroll9646@gmail.com>
2026-01-05 09:12:27 -0300

    Final version

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int totalSheets = scanner.nextInt();
+        int height = 0;
+        int width = 0;
+        int requiredPieces = 0;
+        while (totalSheets --> 0) {
+            width = scanner.nextInt();
+            height = scanner.nextInt();
+            requiredPieces = scanner.nextInt();
+            int sheets = countSheets(width) * countSheets(height);
+            System.out.printf("%s\n",(requiredPieces<=sheets)?"YES":"NO" );
+        }
+    }
+    public static int countSheets(int side){
+        int result = 1;
+        while (side % 2 == 0) {
+            side /= 2;
+            result *= 2;
+        }
+        return result;
     }
 }
